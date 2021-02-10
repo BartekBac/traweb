c07b66048d3ac2cd4e255fd73981e25168d7daa0
DESKTOP-D351Q1F\Bartek <bac2@vp.pl>
2021-02-10 13:40:00 +0100

    add friend hot-fix v2

--- traweb-app/src/app/friends/friends.component.ts
+++ traweb-app/src/app/friends/friends.component.ts
@@ -38,8 +38,7 @@ export class FriendsComponent implements OnInit {
 
     this.userService.updateCurrentUser(userToUpdate).subscribe(
       res => this.toastService.add({severity: 'success', summary: 'You have a new friend!', life: 2000}),
-      err => this.toastService.add({severity: 'error', summary: 'Failed to add a friend.', detail: err, life: 20000, closable: true}),
-      () => this.toastService.add({severity: 'success', summary: 'You have a new friend!', life: 2000}),
+      err => this.toastService.add({severity: 'error', summary: 'Failed to add a friend.', detail: err, life: 20000, closable: true})
     );
   }
 
